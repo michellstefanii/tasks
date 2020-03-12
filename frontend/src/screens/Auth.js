4b691ca50db9965527fca50f9684bf5eaf11b050
Michel S. Stefani <michelstefani@empral.com.br>
2020-03-12 15:01:47 -0300

    availability of the final project back-end and front-end

--- frontend/src/screens/Auth.js
+++ frontend/src/screens/Auth.js
@@ -9,6 +9,7 @@ import {
         } from 'react-native'
 
 import axios from 'axios'
+import AsyncStorage from '@react-native-community/async-storage'
 
 import backgroundImage from '../../assets/imgs/login.jpg'
 import commonStyles from '../commonStyles'
@@ -60,8 +61,9 @@ export default class Auth extends Component {
                 email: this.state.email,
                 password: this.state.password
             })
+                AsyncStorage.setItem('userData', JSON.stringify(res.data))
                 axios.defaults.headers.common['Authorization'] = `bearer ${res.data.token}`
-                this.props.navigation.navigate('Home')
+                this.props.navigation.navigate('Home', res.data)
         } catch(e) {
             showError(e)
         }
