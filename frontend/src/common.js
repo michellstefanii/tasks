db7fcb0b2d8fae90aebe118c90873beb1d47976a
Michel Santos Stefani <37887270+michellstefanii@users.noreply.github.com>
2020-03-20 09:21:21 -0300

    Update common.js

--- frontend/src/common.js
+++ frontend/src/common.js
@@ -1,6 +1,6 @@
 import { Alert, Platform } from 'react-native'
 
-const server = Platform.OS === 'ios' ? 'http://localhost:3000' : 'http://10.0.2.2:3000'
+const server = Platform.OS === 'ios' ? 'https://super-tasks-app.herokuapp.com' : 'https://super-tasks-app.herokuapp.com'
 
 function showError(err) {
     if(err.response && err.response.data) {
